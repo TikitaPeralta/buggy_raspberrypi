cb9116906fccb5d5cc0bdda733dbae937d6d9155
Tikita Peralta <112982478+TikitaPeralta@users.noreply.github.com>
2024-01-10 21:52:30 +0000

    add other files

--- mu_forward_back.py
+++ mu_forward_back.py
@@ -1,4 +1,15 @@
 import warnings # pip install pytest-warnings
 from time import sleep
+import RPI.GPIO as GPIO
 
 warnings.simplefilter('ignore')
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(25, GPIO.IN)
+
+'''
+reading inputs:
+1: GPIO.HIGH | True | 3.3V
+0: GPIO.FALSE | False | 0V
+'''
