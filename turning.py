13d6d1e3c71314c9fb9c8d4d002b535551f381ea
Tikita Peralta <112982478+TikitaPeralta@users.noreply.github.com>
2024-01-12 11:47:48 +0000

    clean up

--- turning.py
+++ turning.py
@@ -1,60 +1,40 @@
 import warnings # pip install pytest-warnings
 from time import sleep
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 warnings.simplefilter('ignore')
 GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BOARD)
 
-print("start")
-
-def wheel_direction(right, left):
-    rf= right[0]
-    rb = right[1]
-
-    lf = left[0]
-    lb = left[1]
-    return rf, rb, lf, lb
-
-def forward(sec, rwheel_f, lwheel_f):
-    GPIO.output(rwheel_f, True)
-    GPIO.output(lwheel_f, True)   
+def forward_backward(sec, right_wheel, left_wheel):
+    GPIO.output(right_wheel, True)
+    GPIO.output(left_wheel, True)   
     #stop
-    time.sleep(sec)
-    GPIO.output(rwheel_f, False)  
-    GPIO.output(lwheel_f, False)  
+    sleep(sec)
+    GPIO.output(right_wheel, False)  
+    GPIO.output(left_wheel, False)  
 
-def backward(sec, rwheel_b, lwheel_b):
-    GPIO.output(rwheel_b, True)
-    GPIO.output(lwheel_b, True)   
+def turn(sec, wheel):
+    GPIO.output(wheel, True)
     #stop
-    time.sleep(sec)
-    GPIO.output(rwheel_b, False)  
-    GPIO.output(lwheel_b, False)  
-
-def right_turn(sec, lwheel_f):
-    GPIO.output(lwheel_f, True)
-    #stop
-    time.sleep(sec)
-    GPIO.output(lwheel_f, False)  
-
-def left_turn(sec, rwheel_f):
-    GPIO.output(rwheel_f, True)
-    #stop
-    time.sleep(sec)
-    GPIO.output(rwheel_f, False)  
+    sleep(sec)
+    GPIO.output(wheel, False)  
 
 def go():
-    r_f, r_b, l_f, l_b = wheel_direction([25,4], [27,22])
-    GPIO.setup(r_f, GPIO.OUT)
-    GPIO.setup(r_b, GPIO.OUT)   # check color
-    GPIO.setup(l_f, GPIO.OUT)  # 
-    GPIO.setup(l_b, GPIO.OUT)  # 
-    forward(4, r_f, l_f)
-    backward(3, r_b, l_b)
-    right_turn(5, l_f)
-    left_turn(2, r_f)
-    exit()
+    print("start")
+    try:
+        r, l = [25, 4], [27, 22]
+        GPIO.setup(r[0], GPIO.OUT)
+        GPIO.setup(r[1], GPIO.OUT)   # check color
+        GPIO.setup(l[0], GPIO.OUT)  # 
+        GPIO.setup(l[1], GPIO.OUT)  # 
+        forward_backward(4, r[0], l[0])
+        forward_backward(3, r[1], l[1])
+        turn(5, l[0]) # right turning
+        turn(2, r[0]) # left turning
+    finally:
+        GPIO.cleanup()
+    
 
 go()
